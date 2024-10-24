3e327c1d3f791e02facb9484d697c8fe828ff41a
Diego Cortes <diegocortes.pinilla@gmail.com>
2024-10-24 01:52:26 -0300

    Avances distintas vistas

--- productos/urls.py
+++ productos/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('', views.lista_productos, name='lista_productos'),  # URL para ver la lista de productos
     path('index/', views.index, name='index'),
     path('login/', views.login, name='login'),
+    path('login_staff/', views.login_staff, name='login_staff'),
+    path('cart/', views.cart, name='cart'),
+    path('post_compra/', views.post_compra, name='post_compra'),
+    path('admin_dashboard/', views.admin_dashboard, name='admin_dashboard'),
 ]
--- productos/views.py
+++ productos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Producto
 
 def lista_productos(request):
@@ -15,7 +15,41 @@ def lista_productos(request):
     return render(request, 'productos/lista_productos.html', {'productos': productos, 'query': query})
 
 def index(request):
+    # Página principal
     return render(request, 'productos/index.html')
 
 def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        # Simulación de autenticación de cliente
+        if username == 'cliente' and password == '1234':  # Datos ficticios
+            return redirect('cart')  # Redirige a la página del carrito
+        else:
+            return render(request, 'productos/login.html', {'error': 'Credenciales inválidas'})
     return render(request, 'productos/login.html')
+
+def login_staff(request):
+    # Login de staff
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        # Sin validación real, solo una simulación de inicio de sesión
+        if username == 'admin' and password == '1234':  # Datos simulados
+            return redirect('admin_dashboard')  # Redirige a la página principal si las credenciales son válidas
+        else:
+            return render(request, 'productos/login_staff.html', {'error': 'Credenciales inválidas'})
+    
+    return render(request, 'productos/login_staff.html')
+
+def cart(request):
+    # Página principal
+    return render(request, 'productos/cart.html')
+
+def post_compra(request):
+    # Página principal
+    return render(request, 'productos/post_compra.html')
+
+def admin_dashboard(request):
+    # Esta vista sería la del dashboard del administrador
+    return render(request, 'productos/admin_dashboard.html')
